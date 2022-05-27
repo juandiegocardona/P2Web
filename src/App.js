e809faacc68887d272c9d7f1e6116f8f9b8028d9
juandiegocardona <jd.cardonad@uniandes.edu.co>
2022-05-27 13:20:13 -0500

    SmartHometitle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { FormattedMessage } from 'react-intl';
 function App() {
   return (
     <div className="container mt-4">
+      <h1 ><center><FormattedMessage id="SmartHome"/></center> </h1>
       <h1><FormattedMessage id="MySpaces"/></h1>
       <Spaces />
     </div>
